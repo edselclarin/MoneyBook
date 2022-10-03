3307b4ff5f184fb80f21fbd46b7f1534bd6bc0c1
Edsel Clarin <edsel.clarin@yahoo.com>
2022-10-02 17:05:58 -0700

    Added Models and SQL scripts.

---
+++ MoneyBook/Models/State.cs
@@ -0,0 +1,17 @@
+﻿namespace MoneyBook.Models
+{
+    public enum StateTypes
+    {
+        Uncleared,
+        Cleared,
+        Reconciled
+    }
+
+    public static class State
+    {
+        public static string GetAlias(this StateTypes type)
+        {
+            return type.ToString()[..1];
+        }
+    }
+}
---
+++ MoneyBook/Models/Transaction.cs
@@ -0,0 +1,19 @@
+﻿namespace MoneyBook.Models
+{
+    public class Transaction
+    {
+        public int TrnsId { get; set; }
+        public DateTime Date { get; set; }
+        public string TrnsType { get; set; }
+        public string RefNum { get; set; }
+        public string Payee { get; set; }
+        public string Memo { get; set; }
+        public string State { get; set; }
+        public decimal Amount { get; set; }
+        public string ExtTrnsId { get; set; }
+        public int InstId { get; set; }
+        public int AcctId { get; set; }
+        public int CatId { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
