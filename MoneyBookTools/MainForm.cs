2b4a9faceb36091034c52176d1bb72ec79705bc3
Edsel Clarin <edsel.clarin@yahoo.com>
2022-10-15 09:41:01 -0700

    Fixed bug where entering the Legder tab always loads transactions of the first account.

--- MoneyBookTools/MainForm.cs
+++ MoneyBookTools/MainForm.cs
@@ -199,7 +199,10 @@ namespace MoneyBookTools
             
             if (tabControl1.SelectedTab == tabLedger)
             {
-                LoadAccounts();
+                if (comboAccounts.DataSource == null)
+                {
+                    LoadAccounts();
+                }
             }
         }
 
