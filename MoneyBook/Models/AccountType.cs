440928daa92b01822ca88dbd5a8fb21b42e9afb8
Edsel Clarin <edsel.clarin@yahoo.com>
2022-12-09 12:31:30 -0800

    Added AccountType.

---
+++ MoneyBook/Models/AccountType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoneyBook.Models
+{
+    public class AccountType
+    {
+        [Key]
+        public int AcctTypeId { get; set; }
+
+        [Required]
+        public string TypeName { get; set; }
+    }
+}
