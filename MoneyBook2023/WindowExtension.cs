b78256720d407ab36cc7cf81e1ec4df96357e8dd
Edsel Clarin <edsel.clarin@yahoo.com>
2023-02-03 18:52:53 -0800

    Created MoneyBook2023 project.

---
+++ MoneyBook2023/WindowExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace MoneyBook2023
+{
+	public static class WindowExtension
+	{
+		public static string ExceptionWindowTitle { get; set; } = "EXCEPTION";
+
+		public static void ShowExceptionMessage(this Window wnd, Exception ex)
+		{
+			MessageBox.Show(wnd, ex.InnerException != null ? ex.InnerException.Message : ex.Message,
+				ExceptionWindowTitle, MessageBoxButton.OK, MessageBoxImage.Error);
+		}
+	}
+}
