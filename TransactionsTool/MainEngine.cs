99b0dc633a8f0e855cbca2d569fa000ffe04b600
Edsel Clarin <edsel.clarin@yahoo.com>
2023-07-22 16:34:05 -0700

    Displayed transactions on a data grid.

--- TransactionsTool/MainEngine.cs
+++ TransactionsTool/MainEngine.cs
@@ -22,16 +22,7 @@ namespace TransactionsTool
 
         public void Init()
         {
-            try
-            {
-                // test
-                var reader = TransactionFileReaderFactory.CreateReader(FileReaderTypes.Chase);
-                reader?.Read(@"C:\Users\Edsel\Downloads\Chase6979_Activity20230722(1).CSV");
-            }
-            catch (Exception ex)
-            {
-                MainExceptionHandler.Instance.Process(ex);
-            }
+            // Add application initialization code here.
         }
     }
 }
--- TransactionsTool/ViewModels/MainViewModel.cs
+++ TransactionsTool/ViewModels/MainViewModel.cs
@@ -1,8 +1,41 @@
 ﻿using Caliburn.Micro;
+using System;
+using System.Collections.ObjectModel;
+using TransactionsTool.Models;
+using TransactionsTool.Readers;
 
 namespace TransactionsTool.ViewModels
 {
-    public class MainViewModel : Screen
+    public class MainViewModel : PropertyChangedBase
     {
+        private ObservableCollection<Transaction> transactions_;
+        public ObservableCollection<Transaction> Transactions
+        {
+            get => transactions_;
+            set
+            {
+                transactions_ = value;
+                NotifyOfPropertyChange();
+            }
+        }
+
+        public MainViewModel()
+        {
+            try
+            {
+                // test
+                var reader = (ChaseFileReader)TransactionFileReaderFactory.CreateReader(FileReaderTypes.Chase);
+                reader?.Read(@"C:\Users\Edsel\Downloads\Chase6979_Activity20230722(1).CSV");
+                Transactions = new ObservableCollection<Transaction>();
+                foreach (var transaction in reader.Transactions) 
+                {
+                    Transactions.Add(transaction);
+                }
+            }
+            catch (Exception ex)
+            {
+                MainExceptionHandler.Instance.Process(ex);
+            }
+        }
     }
 }
