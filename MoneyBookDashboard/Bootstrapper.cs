e120b520438571b8bfb47cd115323d6b51b52323
Edsel Clarin <edsel.clarin@yahoo.com>
2023-07-29 13:01:09 -0700

    Added MoneyBookDashboard project.

---
+++ MoneyBookDashboard/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Caliburn.Micro;
+using MoneyBookDashboard.ViewModels;
+
+namespace MoneyBookDashboard
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override async void OnStartup(object sender, System.Windows.StartupEventArgs e)
+        {
+            await DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
