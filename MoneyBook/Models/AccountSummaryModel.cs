e51e99b94d26a2409998aa02a43eb0b7e0edd6f2
Edsel Clarin <edsel.clarin@yahoo.com>
2023-08-05 11:12:10 -0700

    Fixed bug where AccountSummaries view doesn't refresh after adding, deleting, or modifying a transaction.

--- MoneyBook/Models/AccountSummaryModel.cs
+++ MoneyBook/Models/AccountSummaryModel.cs
@@ -1,10 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace MoneyBook.Models
 {
+    [Keyless]
     public class AccountSummaryModel
     {
-        [Key]
         public int AcctId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
