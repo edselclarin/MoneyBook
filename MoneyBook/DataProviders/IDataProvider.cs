9b955e8df3eb3dd459112a022b1ba8bc082e55fe
Edsel Clarin <edsel.clarin@yahoo.com>
2023-08-05 20:50:26 -0700

    Added AccountSummaryDataProvider nad MoneyBookTest unit test project.

---
+++ MoneyBook/DataProviders/IDataProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace MoneyBook.DataProviders
+{
+    public interface IDataProvider<T>
+    {
+        PagedResponse<T> GetAsync(int skip, int take);
+        T? GetAsync(int id);
+        void UpsertAsync(T item);
+        void DeleteAsync(int id);
+    }
+}
---
+++ MoneyBook/DataProviders/PagedResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace MoneyBook.DataProviders
+{
+    public class PagedResponse<T>
+    {
+        public IEnumerable<T>? Items { get; set; }
+        public int Count { get; set; }
+        public int Total { get; set; }
+        public int Skip { get; set; }
+        public int Take { get; set; }
+    }
+}
