fcd2477a0f6f4a64652bb4b7806594ce5ff88326
Edsel Clarin <edsel.clarin@yahoo.com>
2023-09-23 22:01:50 -0700

    MoneyBookDash: Customized the layout of the list view items.

---
+++ MoneyBookDash/Converters/DateConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MoneyBookDash.Converters
+{
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is DateTime dt ? dt.ToString("MM-dd-yyyy") : string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
