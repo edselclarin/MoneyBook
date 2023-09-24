fc1bfde08035b5460e78dcce57c7ba3dab5d7a49
Edsel Clarin <edsel.clarin@yahoo.com>
2023-09-24 09:51:52 -0700

    MoneyBookDash: Added border color to reminder cards.

---
+++ MoneyBookDash/Converters/TitleToBorderColorConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace MoneyBookDash.Converters
+{
+    public class TitleToBorderColorConverter : IValueConverter
+    {
+        public enum Titles
+        {
+            Overdue,
+            Due,
+            Upcoming,
+            Staged
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string title)
+            {
+                return
+                    title.Equals(Titles.Overdue.ToString()) ? "#ff9081" :
+                    title.Equals(Titles.Due.ToString()) ? "#ffeb55" :
+                    title.Equals(Titles.Upcoming.ToString()) ? "#ffb655" :
+                    title.Equals(Titles.Staged.ToString()) ? "#b0c2ff" :
+                    "#000000";
+            }
+            return "#000000";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
